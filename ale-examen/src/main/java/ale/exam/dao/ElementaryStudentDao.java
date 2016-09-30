90993ad026be4d2a5d0024ce7fdd12ce517f0b64
willysystem <willysystems@gmail.com>
2016-09-30 15:12:11 -0400

    add singleton for ElementaryStudentDao

--- ale-examen/src/main/java/ale/exam/dao/ElementaryStudentDao.java
+++ ale-examen/src/main/java/ale/exam/dao/ElementaryStudentDao.java
@@ -4,7 +4,11 @@ import ale.exam.domain.ElementaryStudent;
 
 public class ElementaryStudentDao extends StudentDao<ElementaryStudent>{
 	
+	static private ElementaryStudentDao instance = new ElementaryStudentDao(); 
 	
+	public static ElementaryStudentDao getInstance(){
+		return instance;
+	}
 	
 
 }
